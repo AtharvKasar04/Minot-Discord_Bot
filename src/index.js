54365b9ba13638f2fe6efb90af5a714be50d0894
AtharvKasar <atharvk752@gmail.com>
2025-02-28 22:26:41 +0530

    minor fixes

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ client.on(Events.InteractionCreate, async interaction => {
         await command.execute(interaction);
     } catch (error) {
         console.error(error);
-        await interaction.reply({ content: "There was an error executing this command!", ephemeral: true });
+        await interaction.reply({ content: "There was an error executing this command!", flags: 64 });
     }
 });
 
